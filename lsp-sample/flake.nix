48b782104c87a9eea3c3514eed8c4a10d97a22e6
Thibault Gagnaux <tgagnaux@gmail.com>
2021-06-02 09:21:02 +0200

    Adds flake for local npm development

---
+++ lsp-sample/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "try-purescript-dev-shell";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-darwin" "x86_64-linux" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs-12_x
+          ];
+        };
+      });
+}
